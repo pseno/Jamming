e38f24cd7b3c41ba1588084e83b88b886eb2b5d4
pseno <pasalicseno@hotmail.com>
2022-01-05 17:10:50 +0100

    modified files

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,15 +5,15 @@ import Track from '../Track/Track';
 class TrackList extends React.Component {
   render() {
     return (
-      <div class="TrackList">
+      <div className="TrackList">
         {this.props.tracks.map((track) => {
           return (
             <Track
               track={track}
               key={track.id}
               onAdd={this.props.onAdd}
-              onRemove={this.props.onRemove}
               isRemoval={this.props.isRemoval}
+              onRemove={this.props.onRemove}
             />
           );
         })}
